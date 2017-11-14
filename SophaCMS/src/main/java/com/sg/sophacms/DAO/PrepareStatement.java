155d7c2600fc83dcdb90b3a01527202ea582bcc0
Rithee Nhep <ritheenhep@gmail.com>
2017-11-13 19:15:00 -0600

    add user prepare statement and some database table.

--- SophaCMS/src/main/java/com/sg/sophacms/DAO/PrepareStatement.java
+++ SophaCMS/src/main/java/com/sg/sophacms/DAO/PrepareStatement.java
@@ -10,5 +10,26 @@ package com.sg.sophacms.DAO;
  * @author ritheenhep
  */
 public class PrepareStatement {
+
+    //user table
+    protected final String SQL_INSERT_INTO_USER
+            = "insert into user "
+            + "(FirstName, LastName, Email, Phone, UserName, Password)values(?,?,?,?,?,?) ";
     
+    protected final String SQUL_UPDATE_USER
+            = "update user set FirstName = ?, LastName = ? , Email = ?, "
+            + "Phone = ?, UserName = ?,  Password = ? ";
+    protected final String SQL_DELETE_USER
+            = "delet from user where userId = ?";
+    
+    protected final String SQL_SELECT_ALL_USER
+            = "select * from user";
+    
+    protected final String SQL_SELECT_USER_BY_ID
+            = "select * from user "
+            + " where userId=?";
+    protected final String SQL_SELECTUSER_BY_NAME
+            ="select u.name from user u "
+            + "where userId = ?";
+
 }
