bef7bc799c3368c2c21bc5c29ea0d51b72cfe294
Rithee Nhep <ritheenhep@gmail.com>
2017-11-13 22:31:58 -0600

    add status table, change from blog post to new post.

--- sphaCMS.sql
+++ sphaCMS.sql
@@ -1,45 +1,51 @@
-drop database if exists WNGA;
-create database  WNGA;
-use wnga;
+DROP DATABASE IF EXISTS WNGA;
+CREATE DATABASE WNGA;
+USE WNGA;
+
+CREATE TABLE IF NOT EXISTS `Status` (
+`StatusId` INT NOT NULL AUTO_INCREMENT,
+`Name` VARCHAR(45) NOT NULL,
+PRIMARY KEY (`StatusId`))
+ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS `User` (
-  `userId` INT NOT NULL AUTO_INCREMENT,
-  `FirstName` VARCHAR(30) NOT NULL,
-  `LastName` VARCHAR(30) NOT NULL,
-  `UserName` VARCHAR(30)NOT NULL,
-  `UserPassword` VARCHAR(250) NOT NULL,
-  `Enabled` TINYINT NOT NULL,
-  PRIMARY KEY (`UserId`),
-  KEY `UserName` (`UserName`))
+`userId` INT NOT NULL AUTO_INCREMENT,
+`FirstName` VARCHAR(30) NOT NULL,
+`LastName` VARCHAR(30) NOT NULL,
+`UserName` VARCHAR(30)NOT NULL,
+`UserPassword` VARCHAR(250) NOT NULL,
+`Enabled` TINYINT NOT NULL,
+PRIMARY KEY (`UserId`),
+KEY `UserName` (`UserName`))
 ENGINE = InnoDB;
 
-CREATE TABLE IF NOT EXISTS `Blog_Post` (
-  `BlogId` INT NOT NULL AUTO_INCREMENT,
-  `Title` VARCHAR(50) NULL,
-  `Content` LONGTEXT NULL,
-  `ImagePath` VARCHAR(255) NULL,
-  `PublishDate` DATE NULL,
-  `ExpDate` DATE NULL,
-  `UserId` INT NOT NULL,
-  `CategoryId` INT NOT NULL,
-  `StatusId` INT NOT NULL,
-  PRIMARY KEY (`BlogId`),
-    FOREIGN KEY (`UserId`)
-    REFERENCES `User` (`UserId`))
+CREATE TABLE IF NOT EXISTS `New_Post` (
+`PostId` INT NOT NULL AUTO_INCREMENT,
+`Title` VARCHAR(50) NULL,
+`Content` LONGTEXT NULL,
+`ImagePath` VARCHAR(255) NULL,
+`PublishDate` DATE NULL,
+`ExpDate` DATE NULL,
+`UserId` INT NOT NULL,
+`StatusId` INT NOT NULL,
+PRIMARY KEY (`PostId`),
+FOREIGN KEY (`UserId`)
+REFERENCES `User` (`UserId`))
 ENGINE = InnoDB;
 
-CREATE TABLE IF NOT EXISTS `New_Feed` (
+CREATE TABLE IF NOT EXISTS `News_Feed` (
 `NewsFeedId` INT NOT NULL AUTO_INCREMENT,
 `Content` VARCHAR(250) NULL,
+`Date` DATE NOT NULL,
 PRIMARY KEY (`NewsFeedId`)
 )ENGINE = InnoDB;
 
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `Authorities` (
-  `Username` VARCHAR(30) NOT NULL,
-  `Authority` VARCHAR(30) NOT NULL,
-    FOREIGN KEY (`Username`)
-    REFERENCES `User` (`Username`),
-    KEY `Username` (`Username`))
+`Username` VARCHAR(30) NOT NULL,
+`Authority` VARCHAR(30) NOT NULL,
+FOREIGN KEY (`Username`)
+REFERENCES `User` (`Username`),
+KEY `Username` (`Username`))
 ENGINE = InnoDB;
 
